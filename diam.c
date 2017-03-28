dae0b8333e9af041dedf4956d92f91af62cc70ea
spell1612 <spell1612@gmail.com>
2017-03-28 20:59:49 +0530

    first

---
+++ diam.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include<math.h>
+
+static char c=4;
+int i,k;
+
+void makespace3(int n)
+{
+    if(n==0)
+     return;
+    printf(" ");
+    makespace3(n-1);
+}
+
+
+void makediamond2(int n)
+{
+  if(k==1&&n==i)
+  {
+    k=0;
+    return;
+  }
+  int j;
+  makespace3(n/2);
+  for(j=i-2*n/2;j>0;j--)
+  {
+    printf("%c",c);
+  }
+  makespace3(n/2);
+  printf("\n");
+  if(n==0||k==1)
+  {
+    k=1;
+    makediamond2(n+2);
+  }
+  else
+    makediamond2(n-2);
+
+}
+
+
+int main() {
+    int i,x;
+	printf("\nEnter a number\n");
+	scanf("%d",&n);
+  if(n%2==0)
+    n--;
+  i=n*n;
+  k=0;
+  makediamond3(i);
+	return 0;
+}
